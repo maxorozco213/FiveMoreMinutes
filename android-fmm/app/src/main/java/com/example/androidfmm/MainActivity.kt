bb4dc8008ea2749cef9f9b31b5f27ddda566e86a
Max <maxorozco213@gmail.com>
2020-09-04 20:20:31 -0700

    Added up button and create alarm button - Incomplete implementation

--- android-fmm/app/src/main/java/com/example/androidfmm/MainActivity.kt
+++ android-fmm/app/src/main/java/com/example/androidfmm/MainActivity.kt
@@ -2,16 +2,37 @@ package com.example.androidfmm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navView: NavigationView
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val toolbarView: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbarView)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    fun onCardClick() {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.nav_create_new_alarm -> {
+            Log.i("MENU", "MENU NAV CLICKED")
+            true
+        }
+        else -> {
+            Log.i("MENU", "WAT")
+            super.onOptionsItemSelected(item)
+        }
     }
 }
